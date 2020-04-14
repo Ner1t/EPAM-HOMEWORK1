752ef46426ee354805d3a30cdd7b9b14a6156fee
Ner1t <47398477+Ner1t@users.noreply.github.com>
2020-04-14 18:43:19 +0300

    Add project files.

---
+++ SUM EVEN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SUM_EVEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] even =  { 1, 2, 3, 4, 5, 6 };
+            for (int i = 1; i < even.Length; i++)
+            {
+                if (even[i] % 2 == 0)
+                {
+                    sum += even[i];
+                    
+                }
+                 
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
